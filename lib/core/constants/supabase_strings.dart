bdfc8a1d9e7a7c09b73cd7b83c2b82c9abc9cc87
mohamed ibrahem <mohamed.data13@gmail.com>
2024-05-03 21:17:40 +0300

    Delete supabase_strings.dart

--- lib/core/constants/supabase_strings.dart
+++
@@ -1,5 +0,0 @@
-class SupabaseStrings {
-  static const String url = 'https://ffocjsftmhjrzxtztjrb.supabase.co';
-  static const String apiKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmb2Nqc2Z0bWhqcnp4dHp0anJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyNzEyMjEsImV4cCI6MjAyODg0NzIyMX0.yKjTPSfj5GcyodVrHxmsGuVO8iXLRO01UmEDOIyv0j8';
-}
